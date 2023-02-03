981574bf0a7b89fbebd2ab84251a58c2b82ac5d3
Dal S Garbuja Pun <dal@garbuja.pun>
2023-02-03 10:59:52 +1000

    Implement search result part 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -30,16 +35,18 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // Get query
     const query = searchView.getQuery();
 
     // populate search results
     await model.loadSearchResults(query);
+
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.error(err);
   }
 };
-controlSearchResults();
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
