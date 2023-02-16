4ca746e2a68809a16e6107e3c7fbffb22c4ff330
Dal S Garbuja Pun <dal@garbuja.pun>
2023-02-17 08:34:51 +1000

    Fix: page doesnot reset to default state of 1 when performing another search

--- src/js/model.js
+++ src/js/model.js
@@ -44,6 +44,7 @@ export const loadSearchResults = async function (query) {
         title: recipe.title,
       };
     });
+    state.search.page = 1;
   } catch (err) {
     console.error(err);
     throw err;
